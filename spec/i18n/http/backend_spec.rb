01435f5f03d1a9b2224165ba031df438ca206e98
Răzvicul <razvicul@Bias-MacBook-Pro.local>
2023-03-24 13:28:28 +0100

    Fix tests

--- spec/i18n/http/backend_spec.rb
+++ spec/i18n/http/backend_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe I18n::Http::Backend do
   it 'has a version number' do
     expect(I18n::Http::Backend::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
